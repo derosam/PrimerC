847e7eaf416e9900f4eee733b3f874a6bc5b318e
David Rosas <drosas2010@gmail.com>
2022-01-27 12:49:26 -0500

    primer xcode

---
+++ PrimerC/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  PrimerC
+//
+//  Created by david on 27/01/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    float s;
+    int w=34;
+    char g='e';
+    printf("Digita un numero flotante\n");
+    scanf("%f",&s);
+    printf("Has digitado %.2f te felicitamos %i , %c\n %.3f",s,w,g,s);
+    return 0;
+}
